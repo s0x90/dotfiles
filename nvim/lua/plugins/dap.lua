00ae2b8fb44ea3d22be9ad08c63af9a2cb8808bb
Vitaliy U. <mail@102web.ru>
2026-02-25 20:25:22 +0500

    FIX dap hot keys

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -1,26 +1,109 @@
 return {
   {
-    "mfussenegger/nvim-dap",
-  },
-
-  { "nvim-neotest/nvim-nio" },
-  
-  {
     "rcarriga/nvim-dap-ui",
     lazy = false,
-    dependencies = { "mfussenegger/nvim-dap" },
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
     config = function()
-    require("dapui").setup()
+      local dap = require("dap")
+      local dapui = require("dapui")
+
+      dapui.setup({
+        layouts = {
+          {
+            elements = {
+              { id = "scopes", size = 0.5 },
+              { id = "breakpoints", size = 0.2 },
+              { id = "stacks", size = 0.2 },
+              { id = "watches", size = 0.1 },
+            },
+            size = 40,
+            position = "left",
+          },
+          {
+            elements = {
+              { id = "repl", size = 0.5 },
+              { id = "console", size = 0.5 },
+            },
+            size = 10,
+            position = "bottom",
+          },
+        },
+      })
+
+ -- Auto open/close
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
     end,
   },
 
   {
     "leoluz/nvim-dap-go",
-    lazy = false,
     ft = "go",
     dependencies = { "mfussenegger/nvim-dap" },
     config = function()
-      require("dap-go").setup()
+      require("dap-go").setup({
+        delve = {
+          path = vim.fn.exepath("dlv"),
+        },
+        dap_configurations = {
+          {
+            type = "go",
+            name = "Debug test (module root)",
+            request = "launch",
+            mode = "test",
+            program = "${fileDirname}",
+            cwd = require("lspconfig.util").root_pattern("go.mod")(vim.fn.getcwd()),
+          },
+        },
+      })
+
+      local dap = require("dap")
+      local map = vim.keymap.set
+      map("n", "<F5>", function() require("dap-go").debug_test() end, { desc = "Debug Test" })
+      map("n", "<F9>", function() dap.toggle_breakpoint() end, { desc = "Toggle Breakpoint" })
+      map("n", "<F10>", function() dap.step_over() end, { desc = "Step Over" })
+      map("n", "<F11>", function() dap.step_into() end, { desc = "Step Into" })
+      map("n", "<F12>", function() dap.step_out() end, { desc = "Step Out" })
+      map("n", "<leader>dr", function() dap.repl.open() end, { desc = "Open REPL" })
+
+      vim.o.signcolumn = "yes"
+      vim.fn.sign_define("DapBreakpoint", {
+        text = "●", -- use a normal dot instead of emoji for max compatibility
+        texthl = "DiagnosticSignError",
+        linehl = "",
+        numhl = "",
+      })
+
+     vim.fn.sign_define("DapStopped", {
+        text = "→",
+        texthl = "DiagnosticSignInfo",
+        linehl = "",
+        numhl = "",
+     })
+
+     vim.fn.sign_define("DapBreakpointRejected", {
+        text = "◌",
+        texthl = "DiagnosticSignHint",
+        linehl = "",
+        numhl = "",
+     })
+
     end,
   },
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    dependencies = { "mfussenegger/nvim-dap" },
+    config = function()
+      require("nvim-dap-virtual-text").setup()
+    end,
+  }
 }
