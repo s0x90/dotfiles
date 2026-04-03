edfdb3c8ae364ce2f2f05cbb9aba441f5f1d7512
Vitaliy U. <mail@102web.ru>
2026-04-03 18:14:01 +0500

    Add close all of buffers except current one key map

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -19,6 +19,7 @@ map("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- c
 map("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 map("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 map("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+map("n", "<leader>bo", "<cmd>%bd|e#|bd#<cr>", { desc = "Close all buffers except current" }) -- close all of buffers except current one
 
 -- telescope map bindings
 map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
