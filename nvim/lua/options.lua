1d6af345080471a194632fea57caf02da2f39ff4
Vitaliy U. <mail@102web.ru>
2026-04-12 18:38:16 +0500

    set relative line numbers by default

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -10,4 +10,5 @@ o.tabstop = 2 -- 2 spaces for tabs (prettier default)
 o.shiftwidth = 2 -- 2 spaces for indent width
 o.expandtab = true -- expand tab to spaces
 o.autoindent = true -- copy indent from current line when starting new one
+o.relativenumber = true
 
